75f36ec1f986b5bd6e8762c3521027cbfd2c6894
Kshitij gupta <kshitijgupta070704@gmail.com>
2024-11-25 19:53:47 +0530

    2 Q of div 3 1996

---
+++ Kshitij Gupta/800/1996B.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        
+        vector<string> grid(n);
+        for (int i = 0; i < n; i++) {
+            cin >> grid[i];
+        }
+        
+        for (int i = 0; i < n; i += k) {  
+            for (int j = 0; j < n; j += k) { 
+                cout << grid[i][j];
+            }
+            cout << endl;
+        }
+    }
+    return 0;
+}
